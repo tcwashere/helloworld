022e73780148cabe157614eedaec2d4035f3ce47
Jon Changkachith <jchangkachith@accuratebackground.com>
2017-02-21 21:51:31 -0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# assign a name for this microservice
+ms_name=demo-ms
+
+echo "Loging into ECR (west)..."
+aws ecr get-login --region us-west-1 | bash
+
+echo "Starting build..."
+project_dir=`find . -maxdepth 2 -name "pom.xml" -print | sed -e 's/.pom.xml$//g'`
+cd $project_dir
+mvn clean install
+
+echo "Building Docker image..."
+cd ..
+docker build -t $ms_name .
+
+echo "Tagging our microservice..."
+docker tag $ms_name:latest 132439413064.dkr.ecr.us-west-1.amazonaws.com/$ms_name:v1
+
+echo "Tagging our microservice..."
+docker push 132439413064.dkr.ecr.us-west-1.amazonaws.com/$ms_name:v1
+
+echo "Done"
---
+++ helloworld/src/main/java/com/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String sayHi() {
+		return "<h1>Hello World, Jon!</h1>";
+	}
+	
+}
